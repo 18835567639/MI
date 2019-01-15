b43a3d1cbac2443a3323367b0c19118d99d941f1
18835567639 <you@example.com>
2019-01-15 08:46:42 +0800

    456

---
+++ js/jiadian.js
@@ -0,0 +1,19 @@
+let topLis = document.querySelectorAll(".household-top2s span");
+        let bottomLis = document.querySelectorAll(".household-bottoms");
+        topLis.forEach(function(val, index) {
+        	//全部添加点击事件
+        	val.onclick = function() {
+
+        		bottomLis.forEach(function(val, key) {
+        			//去掉上面所有li的选择状态
+        			topLis[key].style.color = "#585858";
+        			//全部隐藏下面的li,
+        			val.style.display = "none";
+        		});
+        		//显示对应的下面的li
+        		bottomLis[index].style.display = "block";
+        		//给对应的上面的li上色
+        		topLis[index].style.color = "#ff6700";
+        		// topLis[index].style.border-bottom = "1px solid #ff6700";
+        	}
+        });
